e30d112297d080705371f39f6e1099cd8d2bd772
Steven <servingbaby@users.noreply.github.com>
2023-01-20 20:57:53 -0600

    Update chatgpt.py
    Speech with AWS Polly

--- chatgpt.py
+++ chatgpt.py
@@ -1,6 +1,34 @@
 import openai
 import subprocess
+import boto3
+import pyaudio
+import os
+import sys
 from loguru import logger
+from ctypes import *
+from contextlib import contextmanager
+
+# Sound Error Handling
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+
+def py_jack_error_handler(filename, line, function, err, fmt):
+    pass
+
+def py_error_handler(filename, line, function, err, fmt):
+    pass
+
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+c_jack_error_handler = ERROR_HANDLER_FUNC(py_jack_error_handler)
+
+@contextmanager
+def noalsaerr():
+    asound = cdll.LoadLibrary('libasound.so')
+    asound.snd_lib_error_set_handler(c_error_handler)
+    jack = cdll.LoadLibrary('libjack.so')
+    jack.jack_set_error_function(c_jack_error_handler)
+    yield
+    asound.snd_lib_error_set_handler(None)
+    jack.jack_set_error_function(None)
 
 # Remove the default logger
 logger.remove()
@@ -60,7 +88,7 @@ else:
             break
 
 # Add a variable to control flite usage
-use_flite = input("Do you want to use text-to-speech? (y/n) (default: n): ") or "n"
+use_speech = input("Do you want to use text-to-speech? (y/n) (default: n): ") or "n"
 
 # Prompt the user for their initial input
 print("Select the initial prompt:")
@@ -107,8 +135,30 @@ while True:
     print("AI: " + text)
 
     # Speak the text using flite if use_flite is "y"
-    if use_flite == "y":
-        subprocess.run(["flite", "-voice", "slt", "--setf", "duration_stretch=1.15", "--setf", "int_f0_target_mean=160", "-pw", "-t", text], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if use_speech == "y":
+        # subprocess.run(["flite", "-voice", "slt", "--setf", "duration_stretch=1.15", "--setf", "int_f0_target_mean=160", "-pw", "-t", text], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        #
+        polly = boto3.client('polly')
+        response = polly.synthesize_speech(
+            Engine='neural',    
+            VoiceId='Salli',
+            OutputFormat='pcm',
+            Text = text
+        )
+        
+        audio_stream = response['AudioStream'].read()
+
+        with noalsaerr():
+            p = pyaudio.PyAudio()
+            stream = p.open(format=p.get_format_from_width(width=2),
+                            channels=1,
+                            rate=16000,
+                            output=True)
+            stream.start_stream()
+            stream.write(audio_stream)
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
 
     # Log the conversation
     logger.info("User: {}", initial_prompt)
