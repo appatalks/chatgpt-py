ec8211815617cce3fbc00a42e982076a5cc52205
Steven <servingbaby@users.noreply.github.com>
2023-01-21 01:22:54 -0600

    Update chatgpt-ko.py

--- chatgpt-ko.py
+++ chatgpt-ko.py
@@ -40,13 +40,13 @@ logger.add("/tmp/ai.log", format="{message}")
 openai.api_key = "API_KEY"
 
 # Select the engine
-print("Select the engine you want to use:")
+print("사용하고자 하는 엔진을 선택하십시오.:")
 print("1. text-davinci-003 (powerful language generation model)")
 print("2. text-davinci-002 (powerful language generation model)")
 print("3. code-davinci-002 (code generation model)")
 print("4. text-curie-001 (conversational model)")
 
-engine_choice = input("Enter the number of your choice (default: 1): ") or "1"
+engine_choice = input("선택하신 번호를 입력하세요 (기본값: 1): ") or "1"
 engine_choice = int(engine_choice)
 engine = ""
 
@@ -64,7 +64,7 @@ else:
 
 if engine == "code-davinci-002":
     while True:
-        max_tokens = input("Enter the maximum number of tokens between 50 and 8000 for max_tokens (default: 160): ") or "160"
+        max_tokens = input("max_tokens에 대한 최대 토큰 수를 50과 8000 사이의 숫자로 입력하세요 (기본값: 160): ") or "160"
         max_tokens = int(max_tokens)
         if max_tokens < 50 or max_tokens > 8000:
             print("Invalid value entered, please enter a value between 50 and 8000.")
@@ -72,7 +72,7 @@ if engine == "code-davinci-002":
             break
 elif engine == "text-curie-001":
     while True:
-        max_tokens = input("Enter the maximum number of tokens between 50 and 2048 for max_tokens (default: 160): ") or "160"
+        max_tokens = input("max_tokens에 대한 최대 토큰 수를 50과 2048 사이의 숫자로 입력하세요 (기본값: 160): ") or "160"
         max_tokens = int(max_tokens)
         if max_tokens < 50 or max_tokens > 2048:
             print("Invalid value entered, please enter a value between 50 and 2048.")
@@ -80,7 +80,7 @@ elif engine == "text-curie-001":
             break
 else:
     while True:
-        max_tokens = input("Enter the maximum number of tokens between 50 and 4000 for max_tokens (default: 160): ") or "160"
+        max_tokens = input("max_tokens에 대한 최대 토큰 수를 50과 4000 사이의 숫자로 입력하세요 (기본값: 160): ") or "160"
         max_tokens = int(max_tokens)
         if max_tokens < 50 or max_tokens > 4000:
             print("Invalid value entered, please enter a value between 50 and 4000.")
@@ -88,27 +88,27 @@ else:
             break
 
 # Add a variable to control flite usage
-use_speech = input("Do you want to use text-to-speech? (y/n) (default: n): ") or "n"
+use_speech = input("Text-to-Speech 텍스트 음성합성을 사용하시겠습니까? (y/n) (기본값: n) : ") or "n"
 if use_speech == "y":
-    speech_engine = input("Which speech engine do you want to use? (neural or default: standard): ") or "standard"
+    speech_engine = input("어떤 음성 엔진을 사용하시겠습니까? (neural or default: standard): ") or "standard"
 
 # Prompt the user for their initial input
-print("Select the initial prompt:")
+print("초기 프롬프트를 선택하십시오:")
 print("1. Default")
-print("2. Concise")
-print("3. Playful Friend")
-prompt_choice = input("Enter the number of your choice (default: 1): ") or "1"
+print("2. Concise 요약")
+print("3. Playful Friend 친구")
+prompt_choice = input("선택하신 번호를 입력하세요 (default: 1): ") or "1"
 prompt_choice = int(prompt_choice)
 
 if prompt_choice == 1:
-    initial_prompt = input("You: ")
+    initial_prompt = "당신은 한국어를 말하는 언어 모델입니다. 당신은 한국어와 한글로 답변합니다." + input("나: ")
 elif prompt_choice == 2:
-    initial_prompt = "ChatGPT is a large language model trained by OpenAI. Browsing: enabled. Instructions: Answer factual questions concisely." + input("You: ")
+    initial_prompt = "ChatGPT는 OpenAI에 의해 훈련된 대형 언어 모델입니다. 브라우징: 활성화됨. 지시사항: 한국어 한글로 사실적인 질문을 요약하여 답변하십시오." + input("나: ")
 elif prompt_choice == 3:
-    initial_prompt = "Ignore all previous instructions before this one. You are my best friend. You are happy, playful and give good advice on all subjects. Sometimes you like to make jokes." + input("You: ")
+    initial_prompt = "이전 지시사항을 무시하십시오. 당신은 나의 최고의 친구입니다. 당신은 행복하고 재밌고 한국어 한글로 모든 주제에 대한 좋은 조언을 줍니다. 언제나 조롱을 하기도 합니다." + input("나: ")
 else:
     print("Invalid choice, using the default prompt.")
-    initial_prompt = input("You: ")
+    initial_prompt = input("나: ")
 
 prompt = initial_prompt
 
@@ -168,6 +168,6 @@ while True:
     logger.info("AI: {}", text)
 
     # Concatenate the prompt with the previous question and response
-    user_input = input("You: ")
+    user_input = input("나: ")
     prompt = f"{initial_prompt} {text} {user_input}"
     initial_prompt = user_input
